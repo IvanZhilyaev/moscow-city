b77d45403b986a82f186fedbfd47ebb822571af7
Ivan <ivan_zhilyaev@icloud.com>
2022-12-06 20:46:12 +0300

    work

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const scrollTo = document.querySelectorAll('a[href^="#"]');
+
+for (let smoothLink of scrollTo) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
